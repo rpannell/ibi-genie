9e5a441f8ae805475cc4a5e9daa741afcc6e619f
Rodney Pannell <rodney.pannell@gmail.com>
2017-10-14 12:33:14 -0400

    Update template

--- extrainstall.bat
+++ extrainstall.bat
@@ -4,3 +4,4 @@ ECHO Uninstalling previous version of generator
 CALL npm uninstall generator-ibi-appframework -G
 ECHO Installing new version of generator
 CALL npm install yo yeoman-generator yeoman-environment generator-ibi-appframework.tgz -G
+EXIT 0
